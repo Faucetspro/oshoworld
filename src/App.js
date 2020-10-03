fcff4ae804602eea6bb33e303053d61d75f7c5b8
Dharmendra <admin@faucetspro.com>
2020-10-03 15:32:41 +0530

    Push me

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, {Component} from 'react';
+import React, {Fragment} from 'react';
 import AudioList from './components/audioList/audioList';
+import ParticleBackground from './components/ParticleBG/ParticleBackground'
 import './App.css';
-
-class App extends Component {
-  render(){
-    return (
+const App = () => {
+  return (
+    <Fragment>
+      <ParticleBackground />
       <div className="App">
-        <h4>Osho All Audio Resources</h4>
-        <AudioList />
+          <h4>Osho All Audio Resources</h4>
+          <AudioList />
       </div>
-    );
-  }
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ParticleBG/ParticleBackground.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ParticlesBg from 'particles-bg';
+
+const ParticleBackground = () => {
+    return (
+        <ParticlesBg type="random" bg={true} />
+    )
+}
+
+export default ParticleBackground
